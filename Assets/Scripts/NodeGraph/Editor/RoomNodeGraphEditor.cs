8aef8a2d15ba16cdb544465ed23f671ccfb65081
Phuc <phucvipbn01@gmail.com>
2025-03-26 16:45:36 +0700

    .

---
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -0,0 +1,30 @@
+using UnityEditor;
+using UnityEngine;
+public class RoomNodeGraphEditor : EditorWindow {
+
+    private GUIStyle roomNodeStyle;
+
+    private const float nodeWidth = 160f;
+    private const float nodeHeight = 75f;
+    private const int nodePadding = 25;
+    private const int nodeBorder =  12;
+    [MenuItem("Shooter Hunter/RoomNodeGraph")]
+    private static void ShowWindow() {
+        var window = GetWindow<RoomNodeGraphEditor>();
+        window.titleContent = new GUIContent("RoomNodeGraph");
+        window.Show();
+    }
+
+    private void OnEnable()
+    {
+        roomNodeStyle = new GUIStyle();
+        roomNodeStyle.normal.background = EditorGUIUtility.Load("node1") as Texture2D;
+        roomNodeStyle.normal.textColor = Color.white;
+        roomNodeStyle.padding = new RectOffset(nodePadding,nodePadding,nodePadding,nodePadding);
+        roomNodeStyle.border = new RectOffset(nodeBorder,nodeBorder,nodeBorder,nodeBorder);
+    }
+
+    private void OnGUI() {
+        GUILayout.BeginArea(new Rect(new Vector2(100f,100f),new Vector2(nodeWidth,nodeHeight)),roomNodeStyle);
+    }
+}
